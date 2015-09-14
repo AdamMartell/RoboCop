61b23338bcbab4147abe7b7e3ea6d16b53b634be
unknown <AMartell@dev9.bsecode.inc>
2015-09-14 10:57:45 -0500

    Roundhouse

--- Robot.py
+++ Robot.py
@@ -2,15 +2,28 @@
 class PowerSupply:
 	def __init__ (self):
 		self.power = 100
-	def power_drain (self):
-		print self.power
+	def power_Remaining (self):
+		print "Power remaining: ", self.power
 		
 class Legs:
 	def run(self, powerSupply):
 		powerSupply.power = powerSupply.power - 2
+		print "*RoboCop Runs*"
+	def walk(self, powerSupply):
+		powerSupply.power = powerSupply.power - 1
+		print "*RoboCop Walks*"
+	def roundHouse(self, powerSupply):
+		powerSupply.power = powerSupply.power - 5
+		print "*RoboCop roundhouses a bitch* *Smack!!!!!*"
+		
+class Arms:
 	
 powerSupply = PowerSupply()
 legs = Legs()
-powerSupply.power_drain()
+powerSupply.power_Remaining()
 legs.run(powerSupply)
-powerSupply.power_drain()
+powerSupply.power_Remaining()
+legs.walk(powerSupply)
+powerSupply.power_Remaining()
+legs.roundHouse(powerSupply)
+powerSupply.power_Remaining()
